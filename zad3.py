53bb7d7c8f54a9eaea912bd5b406fbeb940f3158
vlad <bon.jore@mail.ru>
2024-11-17 20:00:06 +0300

    matic

--- zad3.py
+++ zad3.py
@@ -4,9 +4,12 @@ def mult (x,y):
     return(x * y)
 def div (x, y):
     return(x // y)
+def dif(x,y):
+    return(x - y)
 
 
-print(func(x, y))
+
+print(func( x, y))
 print(mult(x, y))
 print(div(x, y))
 
